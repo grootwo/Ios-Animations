a7312068d4969940faaadcf920865fdf81128218
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-05-18 12:22:01 +0900

    Feat: create infinite animation

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -10,16 +10,25 @@ import SwiftUI
 struct ContentView: View {
     @State private var animationAmount = 1.0
     var body: some View {
-        Button("Tap here") {
-            animationAmount += 0.5
-        }
-        .padding(40)
-        .background(.blue)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .blur(radius: (animationAmount - 1) * 2)
-        .scaleEffect(animationAmount)
-        .animation(.snappy, value: animationAmount)
+        Button("Tap here") {}
+            .padding(40)
+            .background(.blue)
+            .foregroundColor(.white)
+            .clipShape(.circle)
+            .overlay(
+                Circle()
+                    .stroke(.blue)
+                    .scaleEffect(animationAmount)
+                    .opacity(2 - animationAmount)
+                    .animation(
+                        .easeInOut(duration: 1.5)
+                        .repeatForever(autoreverses: true),
+                        value: animationAmount
+                    )
+            )
+            .onAppear {
+                animationAmount = 2
+            }
     }
 }
 
