a7cf6ea41bdba398e0626db2aa36f69012b0c243
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-05-18 12:50:29 +0900

    Feat: apply animation to the binding value

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -10,25 +10,19 @@ import SwiftUI
 struct ContentView: View {
     @State private var animationAmount = 1.0
     var body: some View {
-        Button("Tap here") {}
-            .padding(40)
-            .background(.blue)
-            .foregroundColor(.white)
-            .clipShape(.circle)
-            .overlay(
-                Circle()
-                    .stroke(.blue)
-                    .scaleEffect(animationAmount)
-                    .opacity(2 - animationAmount)
-                    .animation(
-                        .easeInOut(duration: 1.5)
-                        .repeatForever(autoreverses: true),
-                        value: animationAmount
-                    )
-            )
-            .onAppear {
-                animationAmount = 2
-            }
+        VStack {
+            Button("Tap here") {}
+                .padding(40)
+                .background(.blue)
+                .foregroundColor(.white)
+                .clipShape(.circle)
+                .scaleEffect(animationAmount)
+            Spacer()
+            Stepper("Amount of animation", value: $animationAmount.animation(
+                .default
+            ), in: 1...5)
+        }
+        .padding()
     }
 }
 
