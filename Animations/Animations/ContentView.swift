a5041b1c1545b5a0c32c732fc69fd74c90c0a160
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-05-18 13:03:37 +0900

    Feat: create 3d rotate animation with withAnimation

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,19 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 1.0
+    @State private var animationAmount = 0.0
     var body: some View {
         VStack {
-            Button("Tap here") {}
-                .padding(40)
+            Button("Tap here") {
+                withAnimation(.spring(duration: 1, bounce: 0.6)) {
+                    animationAmount += 360
+                }
+            }
+                .padding(50)
                 .background(.blue)
                 .foregroundColor(.white)
                 .clipShape(.circle)
-                .scaleEffect(animationAmount)
-            Spacer()
-            Stepper("Amount of animation", value: $animationAmount.animation(
-                .default
-            ), in: 1...5)
+                .rotation3DEffect(
+                    .degrees(animationAmount), axis: (x: 1.0, y: 1.0, z: 1.0))
         }
         .padding()
     }
