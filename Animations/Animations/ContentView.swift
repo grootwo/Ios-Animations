9913dd9d07471731511056878d0a844feb0c0805
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-05-19 12:25:09 +0900

    Feat: create another connected string animation

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,22 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    let letters = Array("i love you")
+    @State private var enabled = false
     @State private var dragAmount = CGSize.zero
+
     var body: some View {
-        RadialGradient(colors: [.cyan, .green, .yellow], center: .center, startRadius: 180, endRadius: 0)
-            .frame(width: 350, height: 350)
-            .clipShape(.rect(cornerRadius: 20))
-            .offset(dragAmount)
-            .gesture(
-                DragGesture()
-                    .onChanged { dragAmount = $0.translation }
-                    .onEnded { _ in
-                        withAnimation(.smooth) {
-                            dragAmount = CGSize.zero
-                        }
-                    }
-            )
-//            .animation(.smooth, value: dragAmount)
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) { num in
+                Text(String(letters[num]))
+                    .font(.title)
+                    .padding(10)
+                    .frame(width: 40, height: 40)
+                    .background(enabled ? .yellow : .green)
+                    .clipShape(.circle)
+                    .offset(dragAmount)
+                    .animation(.linear.delay(Double(num) / 20), value: dragAmount)
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation }
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
     }
 }
 
