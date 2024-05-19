622ca43aca99af7a4c158f74c510cf24ec1773f9
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-05-19 12:45:43 +0900

    Feat: create transition animation

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,31 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    let letters = Array("i love you")
-    @State private var enabled = false
-    @State private var dragAmount = CGSize.zero
+    @State private var isShowingElement = false
 
     var body: some View {
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count, id: \.self) { num in
-                Text(String(letters[num]))
-                    .font(.title)
-                    .padding(10)
-                    .frame(width: 40, height: 40)
-                    .background(enabled ? .yellow : .green)
-                    .clipShape(.circle)
-                    .offset(dragAmount)
-                    .animation(.linear.delay(Double(num) / 20), value: dragAmount)
+        VStack {
+            if isShowingElement {
+                Circle()
+                    .frame(width: 200)
+                    .transition(.asymmetric(insertion: .slide, removal: .push(from: .top)))
             }
-        }
-        .gesture(
-            DragGesture()
-                .onChanged { dragAmount = $0.translation }
-                .onEnded { _ in
-                    dragAmount = .zero
-                    enabled.toggle()
+                
+            Button("Tap Here") {
+                withAnimation {
+                    isShowingElement.toggle()
                 }
-        )
+            }
+            .padding()
+            .buttonStyle(.borderedProminent)
+        }
     }
 }
 
